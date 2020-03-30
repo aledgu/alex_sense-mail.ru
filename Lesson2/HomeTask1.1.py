1d51ad8239a3c8a68f9446610b79900d4d4eac20
Alex <Alex_sense@mail.ru>
2020-03-31 01:35:30 +0300

    Finish_Homework_Lesson1_m

---
+++ Lesson2/HomeTask1.1.py
@@ -0,0 +1,6 @@
+a = 2
+b = "stroka"
+c = 2.25
+vvod = input("введите символ" )
+
+print(a, b, c, vvod)
---
+++ Lesson2/HomeTask1.3.py
@@ -0,0 +1,4 @@
+n = input('Введите число: ')
+k = int(n) + int(n*2) + int(n*3)
+print(k)
+# proverka vneseniya izmeneniy
