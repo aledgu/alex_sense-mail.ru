03521827fefd30848487dc7741a8557ce0772f1a
Alex <Alex_sense@mail.ru>
2020-04-04 15:25:50 +0300

    Right HomeTask2

--- Lesson2/HomeTask2.6.py
+++ Lesson2/HomeTask2.6.py
@@ -1,88 +1,31 @@
-# tovar1 = {'название': 'компьютер', 'цена': 20000, 'количество': 5, 'eд': 'шт.', 'название':'принтер', 'название':'ноут'}
-# names = ('name', 'price', 'quantity')
-all_dict = {}
-poz_dict = {}
-all_names = ['Компьютер', 'принтер', 'сканер']
-all_price = [2000, 345, 345]
-all_quantity = [2, 3, 4]
-all_stic = ["шт", "шт", "шт"]
-all_keys = ['название', 'цена', 'количество', 'ед.изм']
-v = 1
-# i = 0
-s = 0
-tovar_tuple = ()
-tovar_list = []
-# print(all_keys[s])
-# print(all_names[s])
-for i in range(len(all_names)):
-    poz_dict[all_keys[s]] = all_names[i]
-    print(poz_dict)
-    poz_dict[all_keys[s + 1]] = all_price[i]
-    print(poz_dict)
-    poz_dict[all_keys[s + 2]] = all_quantity[i]
-    print(poz_dict)
-    poz_dict[all_keys[s + 3]] = all_stic[i]
-    print(poz_dict)
-    tovar_tuple = (i+1,poz_dict)
-    print(tovar_tuple)
-    tovar_list.append(tovar_tuple)
-print(tovar_list)
-# key_name = input('Vvedite key for name: ')
-# all_keys.append(key_name)
-# name_element = 1
-# name = input(f'Vvedite {key_name} {name_element} producta(press enter for finish): ')
-# while name != '':
-#     all_names.append(name)
-#     name_element += 1
-#     name = input(f'Vvedite {key_name} {name_element} producta(press enter for finish): ')
-# all_dict[key_name] = all_names
-
-# key_price =  input('Vvedite key for price: ')
-# all_keys.append(key_price)
-# price_element = 1
-# price = int(input(f'Vvedite {key_price} {price_element} producta(000 for finish): '))
-# while price != 000:
-#     all_price.append(price)
-#     price_element += 1
-#     price = int(input(f'Vvedite {key_price} {price_element} producta(000 for finish): '))
-# all_dict[key_price] = all_price
-#
-# key_quantity = input('Vvedite key for quantity: ')
-# all_keys.append(key_quantity)
-# quantity_element = 1
-# quantity = int(input(f'Vvedite {key_quantity} {quantity_element} producta(000 for finish): '))
-# while quantity != 000:
-#     all_quantity.append(quantity)
-#     quantity_element += 1
-#     quantity = int(input(f'Vvedite {key_quantity} {quantity_element} producta(000 for finish): '))
-# all_dict[key_quantity] = all_quantity
-#
-# key_stic = input('Vvedite key for stic: ')
-# all_keys.append(key_stic)
-# stic_element = 1
-# stic = input(f'Vvedite {key_stic} {stic_element} producta(end for finish): ')
-# while stic != 'end':
-#     all_stic.append(stic)
-#     stic_element += 1
-#     stic = input(f'Vvedite {key_stic} {stic_element} producta(end for finish): ')
-# all_dict[key_stic] = all_stic
-
-# print(all_names)
-# print(all_price)
-# print(all_quantity)
-# print(all_stic)
+"""условия задачи:
+*Реализовать структуру данных «Товары». Она должна представлять собой список кортежей. Каждый кортеж хранит информацию об отдельном товаре. В кортеже должно быть два элемента — номер товара и словарь с параметрами (характеристиками товара: название, цена, количество, единица измерения). Структуру нужно сформировать программно, т.е. запрашивать все данные у пользователя.
+Пример готовой структуры:
+[
+(1, {“название”: “компьютер”, “цена”: 20000, “количество”: 5, “eд”: “шт.”}),
+(2, {“название”: “принтер”, “цена”: 6000, “количество”: 2, “eд”: “шт.”}),
+(3, {“название”: “сканер”, “цена”: 2000, “количество”: 7, “eд”: “шт.”})
+]
+Необходимо собрать аналитику о товарах. Реализовать словарь, в котором каждый ключ — характеристика товара, например название, а значение — список значений-характеристик, например список названий товаров.
+Пример:
+{
+“название”: [“компьютер”, “принтер”, “сканер”],
+“цена”: [20000, 6000, 2000],
+“количество”: [5, 2, 7],
+“ед”: [“шт.”]
+}
+"""
 
 
-
-# tovar_tuple = ()
-# tovar_list =[]
-# tovar2 = {}
-# for t in range(0, len(all_names):
-#     for i in names:
-#         print('Vvedite', i, t+1, 'producta:')
-#         a = input()
-#         tovar2[i] = a
-#     tovar_tuple = (t+1,tovar2)
-#     print(tovar_tuple)
-#     tovar_list.append(tovar_tuple)
-# print(tovar_list)
+while True:
+    days = 1
+    start_km = int(input('Стартовый результат: '))
+    last_km = int(input('Финальный результат: '))
+    if start_km > last_km:
+        print('Введены неверные данные')
+    else:
+        while start_km < last_km:
+            start_km += start_km * 0.1  # start_km = start_km + (start_km * 0.1)
+            days += 1
+        print('Спортсмен добьется результата за {} дней'.format(days))
+        break
